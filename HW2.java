6432775266229ef8aef7bcc63244c28550bdba2a
Yuliya <ju7iab@gmail.com>
2023-06-28 12:41:22 +0300

    initial commit

---
+++ HW2.java
@@ -0,0 +1,18 @@
+public class HW2 {
+    public static void main(String[] args) {
+
+        LinkedListT <Integer> ListT = new LinkedListT<>();
+
+        ListT.add(5);
+        ListT.add(1);
+        ListT.add(8);
+        ListT.add(15);
+        ListT.add(0);
+        ListT.add(9);
+        ListT.add(7);
+
+        ListT.print();
+        ListT.revers();
+        ListT.print();
+    }
+}
